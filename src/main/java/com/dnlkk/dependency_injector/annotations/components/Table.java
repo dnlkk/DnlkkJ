42753e0313591b9bc298e75c4689efe84dea3419
danilkk <ezzfvkoko@gmail.com>
2023-09-10 15:29:05 +0300

    feat: add @Table tableName

--- src/main/java/com/dnlkk/dependency_injector/annotations/components/Table.java
+++ src/main/java/com/dnlkk/dependency_injector/annotations/components/Table.java
@@ -1,5 +1,12 @@
 package com.dnlkk.dependency_injector.annotations.components;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Table {
-    
+    String tableName();
 }
