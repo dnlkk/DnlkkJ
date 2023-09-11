9be236149e0d42f2b6e34e72f94492e282680db3
danilkk <ezzfvkoko@gmail.com>
2023-09-11 16:10:43 +0300

    feat: add some new functions for repository

--- src/test/java/com/dnlkk/DITest/TestRepository.java
+++ src/test/java/com/dnlkk/DITest/TestRepository.java
@@ -1,5 +1,7 @@
 package com.dnlkk.DITest;
 
+import java.util.List;
+
 import com.dnlkk.dependency_injector.annotations.components.Repository;
 import com.dnlkk.repository.DnlkkRepository;
 
@@ -7,4 +9,6 @@ import lombok.Data;
 
 @Repository
 public interface TestRepository extends DnlkkRepository<Integer, User>{
+    List<User> findByNameAndSurnameOrId(String name, String surname, Integer id);
+    List<User> findByName(String name);
 }
