db6b305603b43b5e5131a27ac3c2fc2f314187d7
danilkk <ezzfvkoko@gmail.com>
2023-09-13 22:56:17 +0300

    feat: add @Query for repos

--- src/test/java/com/dnlkk/DITest/TestRepository.java
+++ src/test/java/com/dnlkk/DITest/TestRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.dnlkk.dependency_injector.annotations.components.Repository;
 import com.dnlkk.repository.DnlkkRepository;
+import com.dnlkk.repository.annotations.Query;
 
 import lombok.Data;
 
@@ -11,4 +12,6 @@ import lombok.Data;
 public interface TestRepository extends DnlkkRepository<Integer, User>{
     List<User> findByNameAndSurnameOrId(String name, String surname, Integer id);
     List<User> findByName(String name);
+    @Query("SELECT * FROM user_table WHERE id = :userId")
+    User testSelect(String userId);
 }
