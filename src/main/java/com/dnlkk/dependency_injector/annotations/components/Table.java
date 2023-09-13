28fff2a69e1ef5047392928207c54117296da345
danilkk <ezzfvkoko@gmail.com>
2023-09-13 22:55:58 +0300

    feat: add @Table scheme

--- src/main/java/com/dnlkk/dependency_injector/annotations/components/Table.java
+++ src/main/java/com/dnlkk/dependency_injector/annotations/components/Table.java
@@ -8,5 +8,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Table {
-    String tableName();
+    String value();
+    String scheme() default "public";
 }
