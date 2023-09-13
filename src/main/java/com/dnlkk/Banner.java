0cf120f3200f5209d315349d5c15fe398c01d519
danilkk <ezzfvkoko@gmail.com>
2023-09-13 22:53:58 +0300

    feat: add dynamic banner + application.yml

---
+++ src/main/java/com/dnlkk/Banner.java
@@ -0,0 +1,60 @@
+package com.dnlkk;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.dnlkk.boot.AppConfig;
+import com.dnlkk.dependency_injector.application_context.ApplicationContext;
+
+public class Banner {
+    private static String banner;
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationContext.class);
+
+    public static String init() {
+        try (Scanner scanner = new Scanner(Banner.class.getClassLoader().getResourceAsStream("banner.txt"))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                stringBuilder.append(line).append("\n");
+            }
+            banner = stringBuilder.toString();
+            banner = inputArgs();
+            return banner;
+        } catch (Exception e) {
+            logger.error("banner.txt in resources not found");
+        }
+        return null;
+    }
+
+    private static String inputArgs() {
+        String version = AppConfig.getProperty("app.version");
+        if (version != null) {
+            String[] versions = version.split("\\.");
+            if (versions.length > 0)
+                banner = banner.replace("%mj", versions[0]);
+            if (versions.length > 1)
+                banner = banner.replace("%mi", versions[1]);
+            if (versions.length > 2)
+                banner = banner.replace("%fx", versions[2]);
+            if (versions.length > 3)
+                banner = banner.replace("%bn", versions[3]);
+        }
+        String name = AppConfig.getProperty("app.name");
+        if (name != null) 
+            banner = banner.replace("%name", name);
+
+        return banner;
+    }
+
+    @Override
+    public String toString() {
+        return banner;
+    }
+    
+}
---
+++ src/main/java/com/dnlkk/boot/AppConfig.java
@@ -0,0 +1,43 @@
+package com.dnlkk.boot;
+
+import java.io.InputStream;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+public class AppConfig {
+    private static Map<String, Object> config;
+    private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    public static String getProperty(String key) {
+        if (config == null) {
+            loadConfig();
+        }
+        return findProperty(config, key);
+    }
+
+    private static String findProperty(Map<String, Object> config, String key) {
+        if (config.containsKey(key))
+            return config.get(key).toString();
+        String[] keys = key.split("\\.");
+        if (keys.length == 0)
+            return null;
+        String keyFirst = keys[0];
+        return findProperty((Map<String,Object>) config.get(keyFirst), key.substring(keyFirst.length() + 1));
+    }
+
+    public static boolean loadConfig() {
+        Yaml yaml = new Yaml();
+        if (config != null)
+            return true;
+        try (InputStream inputStream = AppConfig.class.getClassLoader().getResourceAsStream("application.yml")) {
+            config = yaml.load(inputStream);
+            return config != null;
+        } catch (Exception e) {
+            logger.error("application.yml config in resources not found");
+        }
+        return false;
+    }
+}
