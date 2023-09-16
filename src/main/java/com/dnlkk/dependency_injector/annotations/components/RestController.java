9772cb37d68e6ffa795c74ffeee6956e707595c0
danilkk <ezzfvkoko@gmail.com>
2023-09-16 18:35:06 +0300

    refactor: change path() to value()

--- src/main/java/com/dnlkk/dependency_injector/annotations/components/RestController.java
+++ src/main/java/com/dnlkk/dependency_injector/annotations/components/RestController.java
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RestController {
-    String path() default "/";
+    String value() default "/";
 }
