461e3731055751112f1ed48601a5f6f7a931e3da
danilkk <ezzfvkoko@gmail.com>
2023-09-16 18:37:37 +0300

    feat: add @Param to @Query

--- src/test/java/com/dnlkk/DITest/TestRepository.java
+++ src/test/java/com/dnlkk/DITest/TestRepository.java
@@ -1,19 +1,20 @@
 package com.dnlkk.DITest;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import com.dnlkk.dependency_injector.annotations.components.Repository;
 import com.dnlkk.repository.DnlkkRepository;
+import com.dnlkk.repository.annotations.Param;
 import com.dnlkk.repository.annotations.Query;
-
-import lombok.Data;
 
 @Repository
 public interface TestRepository extends DnlkkRepository<Integer, User>{
     List<User> findByNameAndSurnameOrId(String name, String surname, Integer id);
     List<User> findByName(String name);
     @Query("SELECT * FROM user_table WHERE id = :userId")
-    User testSelect(String userId);
-    Integer countAll();
-    Integer countByName(String name);
+    List<User> testSelect(@Param("userId") Integer userId);
+    Long countAll();
+    Long countByName(String name);
+    BigDecimal sumEarningsByName(String name);
 }
