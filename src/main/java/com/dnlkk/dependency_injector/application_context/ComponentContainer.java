f06c1a1054aecc4e31002eb08bc5837bcdbb239a
danilkk <ezzfvkoko@gmail.com>
2023-09-16 18:42:35 +0300

    feat: getComponents from ComponentContainer

--- src/main/java/com/dnlkk/dependency_injector/application_context/ComponentContainer.java
+++ src/main/java/com/dnlkk/dependency_injector/application_context/ComponentContainer.java
@@ -1,6 +1,9 @@
 package com.dnlkk.dependency_injector.application_context;
 
+import java.util.Map;
+
 public interface ComponentContainer {
     boolean containsComponent(String componentClass);
     Object getComponent(String componentClass);
+    Map<String, Object> getComponents();
 }
