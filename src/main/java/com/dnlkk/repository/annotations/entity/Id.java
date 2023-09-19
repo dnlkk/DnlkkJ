0364e981e054e1f1480cf6860dd68b12798ceac2
danilkk <ezzfvkoko@gmail.com>
2023-09-19 10:45:35 +0300

    feat: add @Id and @Column

--- src/main/java/com/dnlkk/repository/annotations/entity/Id.java
+++ src/main/java/com/dnlkk/repository/annotations/entity/Id.java
@@ -1,5 +1,11 @@
 package com.dnlkk.repository.annotations.entity;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Id {
-    
 }
