59be2709d95679f1a3f66ddcaea21c66da7a943b
danilkk <ezzfvkoko@gmail.com>
2023-09-19 20:00:48 +0300

    refactor: try to generalize and make RepositoryProxyHandler more readable

--- src/main/java/com/dnlkk/repository/RepositoryProxyHandler.java
+++ src/main/java/com/dnlkk/repository/RepositoryProxyHandler.java
@@ -6,37 +6,30 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.tokens.Token.ID;
 
 import com.dnlkk.dependency_injector.DependencyInjector;
 import com.dnlkk.repository.annotations.Param;
 import com.dnlkk.repository.annotations.Query;
-import com.dnlkk.repository.annotations.entity.Column;
-import com.dnlkk.repository.annotations.entity.Id;
 import com.dnlkk.repository.annotations.entity.ManyToMany;
 import com.dnlkk.repository.annotations.entity.ManyToOne;
 import com.dnlkk.repository.annotations.entity.OneToMany;
 import com.dnlkk.repository.annotations.entity.OneToOne;
-import com.dnlkk.repository.annotations.entity.Table;
 import com.dnlkk.repository.dnlkk_connection_pool.DnlkkDataSourceFactory;
 import com.dnlkk.util.EntityUtils;
 import com.dnlkk.util.SQLQueryUtil;
@@ -51,92 +44,41 @@ public class RepositoryProxyHandler implements InvocationHandler {
     private Class<?> valueClass;
     private Class<?> repositoryInterface;
     private List<Field> references;
-
     private final Logger logger = LoggerFactory.getLogger(RepositoryProxyHandler.class);
 
     public RepositoryProxyHandler(Class<?> clazz) {
         repositoryInterface = clazz;
         this.dataSource = DnlkkDataSourceFactory.createDataSource();
-
         references = new ArrayList<>();
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {           
-        List<Object> result = new ArrayList<>();  
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {      
+        Object result = null;
         if (method.isAnnotationPresent(Query.class)) {
-            result = execute(method.getAnnotation(Query.class).value(), args, method.getParameters());
-        }
-        else if (method.getName().startsWith(QueryOperation.FIND.getValue())) {
-            result = executeFind(QueryGenerator.generateQuery(method, tableName, valueClass, references), args);
+            result = executeCustomQuery(method.getAnnotation(Query.class).value(), args, method.getParameters());
+        } else if (method.getName().startsWith(QueryOperation.FIND.getValue())) {
+            result =  executeFindQuery(method, args);
         } else if (method.getName().startsWith(QueryOperation.COUNT.getValue()) || method.getName().startsWith(QueryOperation.SUM.getValue())) {
-            String sql = QueryGenerator.generateQuery(method, tableName, valueClass, references);
-            return executeCount(sql, args);
+            result =  executeCountQuery(method, args);
         } else if (method.getName().equals(QueryOperation.SAVE.getValue())) {
-            return save(args[0]);
+            result = saveEntity(args[0]);
         }
-        if (method.getReturnType() != List.class) {
-            if (!result.isEmpty())
-                return result.get(0);
+        
+        if (result != null && !List.class.isAssignableFrom(method.getReturnType()) && (List.class.isAssignableFrom(result.getClass()))) {
+            if (((List) result).size() > 0)
+                result = ((List) result).get(0);
             else
-                return null;
-        }
-        return method.getReturnType().cast(result);
-    }
-
-
-    public Object executeCount(String sql, Object[] args) throws SQLException {
-        Object result = 0;
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-
-                if (args != null)
-                    for (int i = 0; i < args.length; i++)
-                        statement.setObject(i + 1, args[i]);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next())
-                        result = resultSet.getObject(1);
-                }
-            }
+                result = null;
         }
         return result;
     }
 
-    public List<Object> execute(String sqlWithParams, Object[] args, Parameter[] parameters) {
-        List<Object> result = new ArrayList<>();
-
-        String[] queryParameters = SQLQueryUtil.getParamsFromQuery(sqlWithParams);
-        String sql = SQLQueryUtil.removeParamsFromQuery(sqlWithParams, queryParameters);
-        sql = sql.replace("WHERE", QueryGenerator.getReferencesJoin(references, tableName) + " WHERE");
-        logger.debug(Arrays.toString(queryParameters));
-        logger.debug(sql);
-
-        Object id;
-
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                if (args != null)
-                    for (int i = 0; i < queryParameters.length; i++) {
-                        final int optionalIndex = i;
-                        Optional<Parameter> optional = Arrays.stream(parameters).filter(parameter -> parameter.getAnnotation(Param.class).value().equals(queryParameters[optionalIndex])).findFirst();
-
-                        if (optional.isEmpty()) 
-                            throw new RuntimeException(queryParameters[i] + " not found");
-                        int position = Arrays.stream(parameters).toList().indexOf(optional.get());
-                        statement.setObject(i + 1, args[position]);
-                    }
-                result = statementListExecutor(statement);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
+    // TODO: split method
     public List<Object> statementListExecutor(PreparedStatement statement) throws SQLException {
         List<Object> resultFunction = new ArrayList<>();
         Object id = null; // TODO: id check
-        boolean idChange = false;
+        boolean idChange = false; 
 
         Map<String, Object> oneToMany = new HashMap<>();
         Map<Object, Object> entityToIdMap = new HashMap<>();
@@ -155,7 +97,7 @@ public class RepositoryProxyHandler implements InvocationHandler {
             while (resultSet.next()) {
                 try {
                     for (Field field : fields) {
-                        if (!EntityUtils.isNotId(field)) {
+                        if (!EntityUtils.isNotPK(field)) {
                             Object newId = resultSet.getObject(EntityUtils.getColumnName(field));
                             if (id == null || !id.equals(newId)) {
                                 id = newId;
@@ -253,92 +195,170 @@ public class RepositoryProxyHandler implements InvocationHandler {
         return resultFunction;
     }
 
-    public List<Object> executeFind(String sql, Object[] args) {
-        List<Object> result = new ArrayList<>();
-        
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                if (args != null)
-                    for (int i = 0; i < args.length; i++)
-                        statement.setObject(i + 1, args[i]);
-                result = statementListExecutor(statement);
+ 
+
+    private Object executeCountQuery(Method method, Object[] args) {
+        String sql = QueryGenerator.generateQuery(method, tableName, valueClass, references);
+        Object result = 0;
+    
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+    
+            if (args != null) {
+                for (int i = 0; i < args.length; i++) {
+                    statement.setObject(i + 1, args[i]);
+                }
             }
+    
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    result = resultSet.getObject(1);
+                }
+            }
+    
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    
+        return result;
+    }
+
+    private List<Object> executeCustomQuery(String sqlWithParams, Object[] args, Parameter[] parameters) {
+        List<Object> result = new ArrayList<>();
+    
+        String[] queryParameters = SQLQueryUtil.getParamsFromQuery(sqlWithParams);
+    
+        String sql = SQLQueryUtil.removeParamsFromQuery(sqlWithParams, queryParameters);
+        sql = sql.replace("WHERE", QueryGenerator.getReferencesJoin(references, tableName) + " WHERE");
+    
+        logger.debug(Arrays.toString(queryParameters));
+        logger.debug(sql);
+    
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+    
+            if (args != null) {
+                for (int i = 0; i < queryParameters.length; i++) {
+                    final int optionalIndex = i;
+                    Optional<Parameter> optional = Arrays.stream(parameters)
+                            .filter(parameter -> parameter.getAnnotation(Param.class).value().equals(queryParameters[optionalIndex]))
+                            .findFirst();
+    
+                    if (optional.isEmpty()) {
+                        throw new RuntimeException(queryParameters[i] + " not found");
+                    }
+    
+                    int position = Arrays.stream(parameters).toList().indexOf(optional.get());
+                    statement.setObject(i + 1, args[position]);
+                }
+            }
+    
+            result = statementListExecutor(statement);
+    
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+        return result;
+    }
+    private List<Object> executeFindQuery(Method method, Object[] args) { 
+        String sql = QueryGenerator.generateQuery(method, tableName, valueClass, references);
+        List<Object> result = new ArrayList<>();
+    
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+    
+            if (args != null) {
+                for (int i = 0; i < args.length; i++) {
+                    statement.setObject(i + 1, args[i]);
+                }
+            }
+    
+            result = statementListExecutor(statement);
+    
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
         return result;
     }
     
-    public Object executeSelectByIdQuery(Object id) throws SQLException {
-        Field idField = EntityUtils.getIdField(valueClass);
-        String sql = "SELECT * FROM " + tableName + " WHERE " + EntityUtils.getColumnName(idField) + " = ?";
-            
-        try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.setObject(1, id);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        try {
-                            Object entity = valueClass.getConstructor().newInstance();
+    private Object saveEntity(Object entity) {
+        if (entity == null)
+            return entity;
 
-                            Field[] fields = valueClass.getDeclaredFields();
-                            for (Field field : fields) {
-                                if (EntityUtils.isNotRelation(field)){
-                                    Object retrievedObject = resultSet.getObject(field.getName());
-                                    DependencyInjector.setField(entity, retrievedObject, field);
-                                }
-                            }
-                            return entity;
-                        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-                                | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+        if (List.class.isAssignableFrom(entity.getClass())) {
+            for (Object subEntity : ((List) entity)) {
+                saveEntity(subEntity);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+            return entity;
         }
-        return null;
-    }
-    
-    public Object save(Object entity) {
-        String entityFields = EntityUtils.getColumnNameStream(valueClass)
-            .collect(Collectors.joining(","));
-        String sql = "INSERT INTO " + tableName + " (" + entityFields + ") VALUES (" + EntityUtils.generateQuestionMarks(entityFields) + ")";
 
+        String entityTableName = EntityUtils.getTableName(entity.getClass());
+        String entityFields = EntityUtils.getColumnNameStream(entity.getClass())
+                .collect(Collectors.joining(","));
+        String sql = "INSERT INTO " + entityTableName + " (" + entityFields + ") VALUES (" + EntityUtils.generateQuestionMarks(entityFields) + ")";
+
+    
         try {
-            Field idField = EntityUtils.getIdField(valueClass);
+            Field idField = EntityUtils.getIdField(entity.getClass());
             idField.setAccessible(true);
             Object id = idField.get(entity);
-            if (executeSelectByIdQuery(id) != null){
-                entityFields = EntityUtils.getColumnNameStream(valueClass)
-            .collect(Collectors.joining(" = ?,")) + " = ?";
-                sql = "UPDATE " + tableName + " SET " + entityFields + " WHERE " + EntityUtils.getColumnName(idField) + " = " + id;
-            }
-   
-            try (Connection connection = dataSource.getConnection()) {
-                try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                    int k = 1;
-                    Field[] fields = entity.getClass().getDeclaredFields();
-                    for (Field field : fields) {
-                        if (EntityUtils.isNotId(field) && EntityUtils.isNotRelation(field)){
-                            field.setAccessible(true); 
-                            Object fieldValue = field.get(entity);
-                            statement.setObject(k, fieldValue);
-                            k++;
-                        }
-                    }
-                    statement.executeUpdate();
-                    return entity;
+            List<Object> saveList = new ArrayList<>();
+
+            try {
+                if (id != null) {
+                    entityFields = EntityUtils.getColumnNameStream(entity.getClass())
+                            .collect(Collectors.joining(" = ?,")) + " = ?";
+                    sql = "UPDATE " + entityTableName + " SET " + entityFields + " WHERE " + EntityUtils.getColumnName(idField) + " = ?";
                 }
-            } catch (SQLException | IllegalAccessException e) {
+            } catch (IllegalArgumentException | SecurityException e) {
+                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-        } catch (SecurityException | IllegalArgumentException | SQLException | IllegalAccessException e) {
-            // TODO Auto-generated catch block
+            
+            logger.debug(sql);
+    
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(sql)) {
+    
+                int k = 1;
+                Field[] fields = entity.getClass().getDeclaredFields();
+                for (Field field : fields) {
+                    if (EntityUtils.isNotPK(field) && EntityUtils.isNotFK(field)) {
+                        field.setAccessible(true);
+                        Object fieldValue = field.get(entity);
+                        
+                        if (!EntityUtils.isNotRelation(field)) {
+                            Field fieldFK = EntityUtils.getIdField(field.getType());
+                            fieldFK.setAccessible(true);
+                            fieldValue = fieldFK.get(field.get(entity));
+                        }
+                        
+                        statement.setObject(k, fieldValue);
+                        k++;
+                    }
+                    else if (!EntityUtils.isNotFK(field)) {
+                        field.setAccessible(true);
+                        Object fieldValue = field.get(entity);
+                        saveList.add(fieldValue);
+                    }
+                }
+                if (id != null) {
+                    statement.setObject(k, id);
+                }
+    
+                statement.execute();
+            }
+
+            for (Object fieldValue : saveList) {
+                saveEntity(fieldValue);
+            }
+    
+        } catch (SQLException | IllegalAccessException e) {
             e.printStackTrace();
         }
-        return null;   
+    
+        return entity;
     }
 }
