ea08b4fa8253040b5da79243bc28c71b7b1d77a2
danilkk <ezzfvkoko@gmail.com>
2023-09-19 10:43:42 +0300

    chore: change .sql

--- db/init/data.sql
+++ db/init/data.sql
@@ -6,3 +6,42 @@ VALUES ('dnlkk2', 'fffff2');
 
 INSERT INTO user_table  (name, surname)
 VALUES ('anton', 'antonov');
+
+
+INSERT INTO user_operation_table (amount, operation_user_id)
+VALUES (120.2, 1);
+
+INSERT INTO user_operation_table (amount, operation_user_id)
+VALUES (-20.3, 1);
+
+INSERT INTO user_operation_table (amount, operation_user_id)
+VALUES (22, 2);
+
+INSERT INTO user_operation_table (amount, operation_from_user_id, operation_to_user_id)
+VALUES (103.3, 3, 5);
+
+INSERT INTO user_operation_table (amount, operation_from_user_id, operation_to_user_id)
+VALUES (22, 1, 6);
+
+INSERT INTO user_operation_table (amount, operation_from_user_id, operation_to_user_id)
+VALUES (25, 1, 3);
+
+
+INSERT INTO user_details_table (email, details_user_id) 
+VALUES ('ezzfvkoko@gmail.com', 2);
+
+INSERT INTO user_details_table (email, details_user_id) 
+VALUES ('ezzfvkoko2@gmail.com', 1);
+
+
+INSERT INTO user_doings_table (doing, doings_user_id) 
+VALUES ('do nothing', 2);
+
+INSERT INTO user_doings_table (doing, doings_user_id) 
+VALUES ('do nothing', 1);
+INSERT INTO user_doings_table (doing, doings_user_id) 
+VALUES ('sleep', 1);
+INSERT INTO user_doings_table (doing, doings_user_id) 
+VALUES ('eat', 1);
+INSERT INTO user_doings_table (doing, doings_user_id) 
+VALUES ('eat', 3);
--- db/init/init.sql
+++ db/init/init.sql
@@ -8,3 +8,44 @@ CREATE TABLE IF NOT EXISTS user_table
 
 ALTER TABLE user_table 
 ALTER COLUMN earnings TYPE DECIMAL;
+
+ALTER TABLE user_table 
+DROP COLUMN earnings;
+
+ALTER TABLE user_table 
+ADD COLUMN age INT DEFAULT 18;
+
+drop table if exists user_operation_table;
+CREATE TABLE IF NOT EXISTS user_operation_table
+(
+    operation_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    amount DECIMAL NOT NULL,
+    operation_from_user_id INT NOT NULL REFERENCES user_table(id),
+    operation_to_user_id INT NOT NULL REFERENCES user_table(id)
+);
+
+
+drop table if exists user_incoming_operation_table;
+drop table if exists user_outcoming_operation_table;
+
+ALTER TABLE user_operation_table 
+RENAME COLUMN operation_user_id TO operation_from_user_id;
+
+ALTER TABLE user_operation_table
+ADD COLUMN operation_to_user_id INT NOT NULL DEFAULT 4 REFERENCES user_table(id);
+
+drop table if exists user_details_table;
+CREATE TABLE IF NOT EXISTS user_details_table
+(
+    details_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email VARCHAR(64) NOT NULL,
+    details_user_id INT NOT NULL REFERENCES user_table(id)
+);
+
+drop table if exists user_doings_table;
+CREATE TABLE IF NOT EXISTS user_doings_table
+(
+    doings_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    doing VARCHAR(64) NOT NULL,
+    doings_user_id INT NOT NULL REFERENCES user_table(id)
+);
