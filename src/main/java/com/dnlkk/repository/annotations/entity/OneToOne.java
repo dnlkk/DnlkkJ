accaf42e919f487d0fc7463f23eeb8e6302e4c17
danilkk <ezzfvkoko@gmail.com>
2023-09-19 10:44:57 +0300

    feat: db relation annotations

--- src/main/java/com/dnlkk/repository/annotations/entity/OneToOne.java
+++ src/main/java/com/dnlkk/repository/annotations/entity/OneToOne.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface ManyToMany {
-    
+public @interface OneToOne {
+    String value() default "";
 }
