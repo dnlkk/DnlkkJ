a142128104ce74b6d94fd02e7714a76fd5371d7f
danilkk <ezzfvkoko@gmail.com>
2023-09-27 23:33:30 +0300

    refactor: change removeFirstPath keys[] to splitter

--- src/main/java/com/dnlkk/util/PathUtils.java
+++ src/main/java/com/dnlkk/util/PathUtils.java
@@ -1,6 +1,7 @@
 package com.dnlkk.util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,19 +12,23 @@ public class PathUtils {
         return keys;
     }
 
-    public static String removeFirstPath(String[] keys, String path, int distance) {
-        String keyFirst = keys[0];
-        if (keys.length <= 1)
-            return null;
-        return path.substring(keyFirst.length() + distance);
-    }
-    public static String removeFirstPath(String[] keys, String path) {
-        return removeFirstPath(keys, path, 1);
+    public static String removeFirstPath(String splitter, String[] keys) {        
+        if (keys == null || keys.length <= 1)
+            return "";
+    
+        String[] newArray = Arrays.copyOfRange(keys, 1, keys.length);
+        return String.join(splitter, newArray);
     }
 
     public static String[] regexPath(String splitter, String path) {
+        StringBuilder pathCorrector = new StringBuilder(path);
+        System.out.println(Arrays.toString(splitPath(splitter, path)));
+        if (path.length() > 0 && splitPath(splitter, path).length <= 1 && path.charAt(path.length() - 1) != ('/'))
+            pathCorrector.append("/");
+        String finalPath = pathCorrector.toString();
+        System.out.println(finalPath);
         Pattern pattern = Pattern.compile(splitter);
-        Matcher matcher = pattern.matcher(path);
+        Matcher matcher = pattern.matcher(finalPath);
 
         List<String> paths = new ArrayList<>();
         while (matcher.find())
