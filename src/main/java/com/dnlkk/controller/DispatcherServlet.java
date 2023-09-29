ff6ea70b63cc0537fd2bae5518e10566e5434943
danilkk <ezzfvkoko@gmail.com>
2023-09-29 18:00:44 +0300

    refactor: move get params from request into ControllerUtils.java + add logger.error for wrong param or body

--- src/main/java/com/dnlkk/controller/DispatcherServlet.java
+++ src/main/java/com/dnlkk/controller/DispatcherServlet.java
@@ -3,14 +3,12 @@ package com.dnlkk.controller;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import com.dnlkk.controller.annotations.*;
 import com.dnlkk.controller.responses.ResponseEntity;
 import com.dnlkk.util.ControllerUtils;
-import com.dnlkk.util.EntityUtils;
 import com.dnlkk.util.PathUtils;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -18,10 +16,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Getter
 public class DispatcherServlet extends HttpServlet {
     private final ControllerRegistry controllerRegistry;
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
     public DispatcherServlet() {
         controllerRegistry = new ControllerRegistry();
@@ -38,8 +39,6 @@ public class DispatcherServlet extends HttpServlet {
 
         String path = request.getPathInfo();
         if (path == null || !dispatch(response, request, parameterMap)) {
-            // Если путь не соответствует ни одному из обработчиков, вы можете вернуть
-            // код состояния 404 или выполнить другие действия по умолчанию.
             response.setStatus(404);
             response.getWriter().write("{\"error\": \"Ресурс не найден!\"}");
         }
@@ -49,97 +48,61 @@ public class DispatcherServlet extends HttpServlet {
         String path = request.getPathInfo();
 
         Optional<Map.Entry<String, Object>> optionalEntryMapController = controllerRegistry.getControllers().entrySet().stream()
-            .filter(entryMapController -> path.startsWith(entryMapController.getKey())).findFirst();
+                .filter(entryMapController -> path.startsWith(entryMapController.getKey())).findFirst();
 
         if (optionalEntryMapController.isEmpty())
             return false;
 
         Map.Entry<String, Object> entryMapController = optionalEntryMapController.get();
 
-        String methodPath = entryMapController.getKey().length() == path.length() 
-            ? 
-            "/" 
-            : 
-            path.substring(entryMapController.getKey().length(), path.length());
+        String methodPath = entryMapController.getKey().length() == path.length()
+                ?
+                "/"
+                :
+                path.substring(entryMapController.getKey().length());
 
         Object controller = entryMapController.getValue();
-        
-        if (controller != null){
+
+        if (controller != null) {
             return !Arrays.stream(controller.getClass().getDeclaredMethods())
-                .filter(controllerEndpoint -> {
-                    String requestMapping = getRequestMapping(controllerEndpoint);
-                    if (requestMapping == null)
-                        return false;
-                    if (PathUtils.isRequestMapping(methodPath, requestMapping)) {
-                        if (ControllerUtils.methodEquals(controllerEndpoint, Get.class, request.getMethod()))
+                    .filter(controllerEndpoint -> {
+                        String requestMapping = getRequestMapping(controllerEndpoint);
+                        if (requestMapping == null)
                             return false;
-                        else if (ControllerUtils.methodEquals(controllerEndpoint, Post.class, request.getMethod()))
-                            return false;
-                        else try {
-                                List<Object> parameters = new ArrayList<>();
-                                List<String> requestPaths = Arrays.stream(PathUtils.splitPath("/", methodPath)).toList();
-                                List<String> requestMappingPaths = Arrays.stream(PathUtils.splitPath("/", requestMapping)).toList();
-
-                                for (Parameter parameter : controllerEndpoint.getParameters()) {
-                                    if (parameter.isAnnotationPresent(RequestParam.class)
-                                            && parametersMap.containsKey(parameter.getAnnotation(RequestParam.class).value())) {
-                                        Object[] params = parametersMap.get(parameter.getAnnotation(RequestParam.class).value())[0].split(",");
-                                        if (!parameter.getType().isArray() && params.length == 1)
-                                            if (parameter.getType().equals(Integer.class))
-                                                parameters.add(Integer.parseInt((String) params[0]));
-                                            else
-                                                parameters.add(parameter.getType().cast(params[0]));
-                                        else {
-                                            Object[] returnObject = Arrays.copyOf(params, params.length);
-                                            if (parameter.getType().getComponentType().equals(Integer.class)) {
-                                                returnObject = new Integer[params.length];
-                                                for (int i = 0; i < returnObject.length; i++) {
-                                                    returnObject[i] = Integer.parseInt((String) params[i]);
-                                                } 
-                                            }
-                                            parameters.add(returnObject);
-                                        }
-
-                                    }
-                                    else if (controllerEndpoint.isAnnotationPresent(Post.class)
-                                            && parameter.isAnnotationPresent(RequestBody.class)) {
-                                        parameters.add(EntityUtils.objectMapper.readValue(
-                                                request.getReader().lines().collect(Collectors.joining(System.lineSeparator())),
-                                                parameter.getType())
-                                        );
-                                    }
-                                    else if (parameter.isAnnotationPresent(PathVar.class)) {
-                                        int index = requestMappingPaths.indexOf(String.format(":%s", parameter.getAnnotation(PathVar.class).value()));
-                                        if (index == -1)
-                                            continue;
-                                        if (parameter.getType().equals(Integer.class))
-                                            parameters.add(Integer.parseInt(requestPaths.get(index)));
-                                        else
-                                            parameters.add(requestPaths.get(index));
-                                    }
-                                }
-                            Object controllerReturn = controllerEndpoint.invoke(controller, parameters.toArray());
-                            if (controllerReturn.getClass().equals(ResponseEntity.class)) {
-                                ResponseEntity responseEntity = (ResponseEntity) controllerReturn;
-                                response.setStatus(responseEntity.getStatus().code());
-
-                                response.getWriter().write(responseEntity.json());
-                            } else if (controllerReturn.getClass().equals(String.class)) {
-                                response.getWriter().write(controllerReturn.toString());
-                            } else {
+                        if (PathUtils.isRequestMapping(methodPath, requestMapping)) {
+                            if (ControllerUtils.methodEquals(controllerEndpoint, Get.class, request.getMethod()))
                                 return false;
-                            }
-                            return true;
-                        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
+                            else if (ControllerUtils.methodEquals(controllerEndpoint, Post.class, request.getMethod()))
+                                return false;
+                            else try {
+                                    String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+                                    List<Object> parameters = ControllerUtils.getParametersFromRequest(
+                                            controllerEndpoint,
+                                            methodPath,
+                                            requestMapping,
+                                            parametersMap,
+                                            body
+                                    );
+
+                                    Object controllerReturn = controllerEndpoint.invoke(controller, parameters.toArray());
+                                    if (controllerReturn.getClass().equals(ResponseEntity.class)) {
+                                        ResponseEntity<?> responseEntity = (ResponseEntity<?>) controllerReturn;
+                                        response.setStatus(responseEntity.getStatus().code());
+
+                                        response.getWriter().write(responseEntity.json());
+                                    } else if (controllerReturn.getClass().equals(String.class)) {
+                                        response.getWriter().write(controllerReturn.toString());
+                                    } else {
+                                        return false;
+                                    }
+                                    return true;
+                                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException |
+                                         IOException e) {
+                                    logger.error(e.getMessage());
+                                }
                         }
-                    }
-                    return false;
-                }).toList().isEmpty();
+                        return false;
+                    }).toList().isEmpty();
         }
         return false;
     }
--- src/main/java/com/dnlkk/util/ControllerUtils.java
+++ src/main/java/com/dnlkk/util/ControllerUtils.java
@@ -1,14 +1,71 @@
 package com.dnlkk.util;
 
-import com.dnlkk.controller.annotations.Get;
+import com.dnlkk.controller.annotations.PathVar;
+import com.dnlkk.controller.annotations.Post;
+import com.dnlkk.controller.annotations.RequestBody;
+import com.dnlkk.controller.annotations.RequestParam;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class ControllerUtils {
     public static boolean methodEquals(Method controllerEndpoint, Class<? extends Annotation> methodClass, String requestType) {
         boolean requestEquals = requestType.equals(methodClass.getSimpleName().toUpperCase());
         return ((controllerEndpoint.isAnnotationPresent(methodClass) && !requestEquals) ||
                 (!controllerEndpoint.isAnnotationPresent(methodClass) && requestEquals));
+    }
+
+    public static List<Object> getParametersFromRequest(Method controllerEndpoint, String methodPath, String requestMapping, Map<String, String[]> parametersMap, String body) throws JsonProcessingException {
+        List<Object> parameters = new ArrayList<>();
+        List<String> requestPaths = Arrays.stream(PathUtils.splitPath("/", methodPath)).toList();
+        List<String> requestMappingPaths = Arrays.stream(PathUtils.splitPath("/", requestMapping)).toList();
+
+        for (Parameter parameter : controllerEndpoint.getParameters()) {
+            if (parameter.isAnnotationPresent(RequestParam.class)
+                    && parametersMap.containsKey(parameter.getAnnotation(RequestParam.class).value())) {
+                Object[] params = parametersMap.get(parameter.getAnnotation(RequestParam.class).value())[0].split(",");
+                if (!parameter.getType().isArray() && params.length == 1)
+                    parameters.add(parameterCast(parameter, params[0]));
+                else {
+                    Object[] returnObject = Arrays.copyOf(params, params.length);
+                    if (parameter.getType().getComponentType().equals(Integer.class)) {
+                        returnObject = new Integer[params.length];
+                        for (int i = 0; i < returnObject.length; i++) {
+                            returnObject[i] = Integer.parseInt((String) params[i]);
+                        }
+                    }
+                    parameters.add(returnObject);
+                }
+
+            } else if (controllerEndpoint.isAnnotationPresent(Post.class)
+                    && parameter.isAnnotationPresent(RequestBody.class)) {
+                parameters.add(EntityUtils.objectMapper.readValue(
+                        body,
+                        parameter.getType())
+                );
+            } else if (parameter.isAnnotationPresent(PathVar.class)) {
+                int index = requestMappingPaths.indexOf(String.format(":%s", parameter.getAnnotation(PathVar.class).value()));
+                if (index == -1)
+                    continue;
+                if (parameter.getType().equals(Integer.class))
+                    parameters.add(Integer.parseInt(requestPaths.get(index)));
+                else
+                    parameters.add(requestPaths.get(index));
+            }
+        }
+        return parameters;
+    }
+
+    public static Object parameterCast(Parameter parameter, Object param) {
+        if (parameter.getType().equals(Integer.class))
+            return Integer.parseInt((String) param);
+        else
+            return parameter.getType().cast(param);
     }
 }
